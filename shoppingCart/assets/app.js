b95a109c0638d6037f19c8ad96803966849ed749
ensarvragic <vragicensar@gmail.com>
2023-03-29 02:54:21 +0200

    added counter for items and empty btn

--- shoppingCart/assets/app.js
+++ shoppingCart/assets/app.js
@@ -1,3 +1,18 @@
-productsCart = document.getElementById('Products')
-addToCartBtn = document.getElementById('addToCart')
-let cartItems = [];
+const counter = document.getElementById('cart-counter');
+
+const cart = [];
+
+const addToCart = (item) => {
+    cart.push(item);
+    updateCartCounter();
+}
+
+const emptyCart = () => {
+    cart.length = 0;
+    updateCartCounter();
+}
+
+const updateCartCounter = () => {
+    const counter = document.getElementById('cart-counter');
+    counter.innerHTML = cart.length;
+}
